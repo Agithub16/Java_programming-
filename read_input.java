070b6c84c628f862233797ff9f9bab8880175161
Ayush Vardhan <ayushvardhan08538@gmail.com>
2026-03-09 18:56:19 +0530

    Input_output

---
+++ read_input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class read_input {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+     System.out.println("Enter your string");
+     /*     int a = sc.nextInt();
+        System.out.println(a);
+        boolean b1 = sc.hasNextInt();
+        System.out.println(b1);*/
+        String str = sc.nextLine();
+        System.out.println(str);
+
+
+    }
+
+}
