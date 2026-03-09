33ad321b7eea2ace33070811fcc01f2c37802028
Ayush Vardhan <ayushvardhan08538@gmail.com>
2026-03-09 15:54:46 +0530

    literals in java

---
+++ Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+        int age = 25; //  integer literal
+        double pi = 3.14; // double literal
+        char grade = 'A'; // character literal
+        String name ="Ayush"; // string literal
+        boolean strong = true; // boolean literal
+        System.out.println("age is " +age);
+        System.out.println(+pi);
+        System.out.println(grade);
+        System.out.println(name);
+        System.out.println(strong);
+    }
+    
+}
